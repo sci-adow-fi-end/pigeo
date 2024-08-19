e531751ac7e483070ef7de4b43427be81cbda66a
sci_adow_fi_end <alessio.delli@edu.unifi.it>
2024-08-19 20:54:47 +0200

    added tls

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,51 @@
+use core::panic;
+use log::{info, warn};
+use openssl::ssl::{SslAcceptor, SslConnector, SslFiletype, SslMethod};
+use simple_logger::SimpleLogger;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::sync::mpsc;
+use std::sync::Arc;
 use std::thread;
+
 //use std::time::Duration;
 
-fn main() -> std::io::Result<()> {
+fn main() {
+    //init of the logger
+    SimpleLogger::new().init().unwrap();
+
+    //init of the sinchronization channel
     let (tx, rx) = mpsc::channel();
 
+    //__________________________________________________________________________________CLIENT SIDE
     let client = thread::spawn(move || {
-        println!("il client è nato");
+        info!("the client spawned");
+
         let _response: bool = rx.recv().unwrap();
-        let mut stream = TcpStream::connect("127.0.0.1:3000").unwrap();
+        let mut connector_builder = SslConnector::builder(SslMethod::tls()).unwrap();
+        // FIXME do things proprely
+
+        connector_builder.set_ca_file("certs.pem").unwrap();
+        info!("you are using a self signed certificate, only for testing pourposes");
+
+        let connector = connector_builder.build();
+
+        let stream = match TcpStream::connect("localhost:3000") {
+            Ok(res) => res,
+            Err(_e) => {
+                warn!("the client cannot connect to the server");
+                panic!();
+            }
+        };
+
+        let mut stream = match connector.connect("localhost", stream) {
+            Ok(res) => res,
+            Err(_e) => {
+                warn!("the client cant have a secure connection, aborting");
+                panic!();
+            }
+        };
 
         let message = b"yabadabadooo!";
         stream.write_all(message).unwrap();
@@ -31,44 +65,90 @@ fn main() -> std::io::Result<()> {
         println!("ho finitoo");
     });
 
+    //__________________________________________________________________________________SERVER SIDE
     let server = thread::spawn(move || {
-        println!("il server è nato");
+        info!("the server spawned");
+
+        let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+
+        match acceptor.set_private_key_file("key.pem", SslFiletype::PEM) {
+            Ok(_res) => {}
+            Err(_e) => {
+                warn!("key file not found");
+                panic!();
+            }
+        }
+
+        match acceptor.set_certificate_chain_file("certs.pem") {
+            Ok(_res) => {}
+            Err(_e) => {
+                warn!("certificate file not found");
+                panic!();
+            }
+        }
+
+        match acceptor.check_private_key() {
+            Ok(_res) => {}
+            Err(_e) => {
+                warn!("private key not valid");
+                panic!();
+            }
+        }
+
+        let acceptor = Arc::new(acceptor.build());
+
+        let listener = match TcpListener::bind("localhost:3000") {
+            Ok(res) => res,
+            Err(_e) => {
+                warn!("the server cannot bind correctly");
+                panic!();
+            }
+        };
+
         let ready: bool = true;
         tx.send(ready).unwrap();
 
-        let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
         for stream in listener.incoming() {
-            let mut stream = stream.unwrap();
+            match stream {
+                Ok(stream) => {
+                    let acceptor = acceptor.clone();
 
-            let mut buffer = [0; 1024]; // buffer di 1024 byte
-            let bytes_read = stream.read(&mut buffer).unwrap();
+                    thread::spawn(move || {
+                        let mut stream = acceptor.accept(stream).unwrap();
+                        let mut buffer = [0; 1024]; // buffer di 1024 byte
+                        let bytes_read = stream.read(&mut buffer).unwrap();
 
-            println!(
-                "Ricevuti {} byte: {}",
-                bytes_read,
-                buffer
-                    .iter()
-                    .take(bytes_read)
-                    .map(|&n| n as char)
-                    .collect::<String>()
-            );
+                        println!(
+                            "Ricevuti {} byte: {}",
+                            bytes_read,
+                            buffer
+                                .iter()
+                                .take(bytes_read)
+                                .map(|&n| n as char)
+                                .collect::<String>()
+                        );
 
-            let response = b"message saved";
-            stream.write_all(response).unwrap();
+                        let response = b"message saved";
+                        stream.write_all(response).unwrap();
 
-            println!(
-                "Risposta inviata: {}",
-                buffer
-                    .iter()
-                    .take(bytes_read)
-                    .map(|&n| n as char)
-                    .collect::<String>()
-            );
+                        println!(
+                            "Risposta inviata: {}",
+                            buffer
+                                .iter()
+                                .take(bytes_read)
+                                .map(|&n| n as char)
+                                .collect::<String>()
+                        );
+                    });
+                }
+                Err(_e) => {
+                    warn!("error accepting the connection");
+                }
+            }
         }
     });
+
+    //__________________________________________________________________________________OTHER STUFF
     server.join().unwrap();
     client.join().unwrap();
-    //stream.write(&[1])?;
-    //stream.read(&mut [0; 128])?;
-    Ok(())
 }
